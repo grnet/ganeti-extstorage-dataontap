bd70a201e4c33ab4a8f550af5a93c84676d6d0a1
Nikos Skalkotos <skalkoto@grnet.gr>
2016-08-04 11:40:41 +0300

    Bump development version to 0.1
    Comply with devflow 0.13

--- extstorage_dataontap/version.py
+++ extstorage_dataontap/version.py
@@ -1,8 +1,2 @@
 
-__version__ = "0.0next_36_1bcbc0b"
-__version_vcs_info__ = {
-    'branch': 'develop',
-    'revid': '1bcbc0b',
-    'revno': 36}
-__version_user_email__ = "skalkoto@grnet.gr"
-__version_user_name__ = "Nikos Skalkotos"
+__version__ = "0.1.dev0"
