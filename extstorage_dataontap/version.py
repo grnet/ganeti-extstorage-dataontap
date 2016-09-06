11a7462a28b9255427c5f8f3876931ab4f15a5b4
Nikos Skalkotos <skalkoto@grnet.gr>
2016-09-06 15:30:30 +0300

    Bump development version to 0.2

--- extstorage_dataontap/version.py
+++ extstorage_dataontap/version.py
@@ -1,8 +1,3 @@
 
-__version__ = "0.1"
-__version_vcs_info__ = {
-    'branch': 'master',
-    'revid': '30bde37',
-    'revno': 66}
-__version_user_email__ = "skalkoto@grnet.gr"
-__version_user_name__ = "Nikos Skalkotos"
+__version__ = "0.2dev0"
+
